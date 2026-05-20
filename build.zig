cfa193bd0eb2fcee2231712333e98934a28ce26f
Jassiel Ovando <jassielovando@protonmail.com>
2026-05-20 18:42:44 -0400

    feat: enhance lint step with improved lizard command formatting

--- build.zig
+++ build.zig
@@ -118,7 +118,23 @@ pub fn build(b: *std.Build) void {
     test_step.dependOn(&run_integration_tests.step);
 
     const lint_step = b.step("lint", "Run linters and code quality tools");
-    const lizard = b.addSystemCommand(&.{ "lizard", "-l", "zig", "-C", "10", "-L", "60", "-a", "5", "-m", "-w", "-ENS", "-T", "max_nested_structures=3", "src/" });
+    const lizard = b.addSystemCommand(&.{
+        "lizard",
+        "-l",
+        "zig",
+        "-C",
+        "10",
+        "-L",
+        "60",
+        "-a",
+        "5",
+        "-m",
+        "-w",
+        "-ENS",
+        "-T",
+        "max_nested_structures=3",
+        "src/",
+    });
     lint_step.dependOn(&lizard.step);
     const fmt = b.addFmt(.{
         .check = true,
